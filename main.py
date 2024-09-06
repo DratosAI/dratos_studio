b1523a8de6da7ee94eb34fa669e0fc2a9b69be02
Richard Anthony Hein <richard.hein@gmail.com>
2024-09-06 04:41:38 +0000

    work with organizations

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ import os
 load_dotenv()
 
 LIGHTNING_API_KEY = os.getenv("LIGHTNING_API_KEY")
-LIGHTNING_USER_ID = os.getenv("LIGHTNING_USER_ID")
+# LIGHTNING_USER_ID = "entangleit"
 LIGHTNING_ORG_ID = os.getenv("LIGHTNING_ORG_ID")
 LIGHTNING_TEAMSPACE_ID = os.getenv("LIGHTNING_TEAMSPACE_ID")
 LIGHTNING_STUDIO_ID = os.getenv("LIGHTNING_STUDIO_ID")
 print(LIGHTNING_STUDIO_ID)
 print(LIGHTNING_TEAMSPACE_ID)
-print(LIGHTNING_USER_ID)
+# print(LIGHTNING_USER_ID)
 print(LIGHTNING_ORG_ID)
 
-# allows interaction with the Studio named "my-studio" in the Teamspace "train-model" of user "zeus"
-s = Studio(LIGHTNING_STUDIO_ID, teamspace=LIGHTNING_TEAMSPACE_ID, user=LIGHTNING_USER_ID)
+# allows interaction with the Studio in the Teamspace of org
+s = Studio(LIGHTNING_STUDIO_ID, teamspace=LIGHTNING_TEAMSPACE_ID, org=LIGHTNING_ORG_ID)
 
-# allows interaction with Teamspace "train-model" of organization "olymp"
-t = Teamspace(LIGHTNING_TEAMSPACE_ID, user=LIGHTNING_USER_ID)
+# allows interaction with Teamspace of organization
+t = Teamspace(LIGHTNING_TEAMSPACE_ID, org=LIGHTNING_ORG_ID)
 
-# allows interaction with user "zeus"
-u = User(LIGHTNING_USER_ID)
-print(u.teamspaces)
-# allows interaction with organization "olymp"
+# allows interaction with user
+# u = User("entangleit")
+# print(u.teamspaces)
+# allows interaction with organization
 o = Organization(LIGHTNING_ORG_ID)
 
-print(s.name)
+print("Organization Teamspaces:\n")
+print(o.teamspaces)
+
+print("Studio name: " + s.name)
